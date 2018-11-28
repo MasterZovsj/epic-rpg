e20e623e342c65c75fecb558da7d636fce6140a7
MasterZovsj <44856746+MasterZovsj@users.noreply.github.com>
2018-11-28 00:13:38 -0500

    Created Room Generation
    Five rooms now generate along with the map. The room size and location is random while never going over the edge of the map. Rooms are saved in a cell array called 'Rooms' that can be used to edit the indvidual rooms to add treasure or monsters

--- buildMap.m
+++ buildMap.m
@@ -1,50 +1,75 @@
 function [map] = buildMap()
+%% TODO
+%Make hallways to rooms that do not touch
+%Add Doors between rooms
+%%
 %BUILDMAP v2.0 Creates the base map and rooms
 %   Detailed explanation goes here
-
+%#ok<*AGROW> 
 %define the total map reigon
-map_width = 120;
-map_height = 45;
+mapColumns = 120;
+mapRows = 45;
 
-for j=1:map_width
-    for i=1:map_height
-        if i==1
-            map(i,j)='-'; %used on the upper border
-        elseif i==map_height
-            map(i,j)='-'; %used on lower border
-        elseif j==1
-            map(i,j)='|'; %used on left border
-        elseif j==map_width
-            map(i,j)='|'; %used on right border
+for jj=1:mapColumns
+    for ii=1:mapRows
+        if ii==1
+            map(ii,jj)='-'; %used on the upper border
+        elseif ii==mapRows
+            map(ii,jj)='-'; %used on lower border
+        elseif jj==1
+            map(ii,jj)='|'; %used on left border
+        elseif jj==mapColumns
+            map(ii,jj)='|'; %used on right border
         else
-            map(i,j)=' '; %used everywhere else
+            map(ii,jj)=' '; %used everywhere else
         end
     end
 end
-
 %% generate a room
-room_num=5;
-for k=1:room_num
-room_height = randi([5,15]);
-room_width = randi([5,30]);
-x_start = randi([2,map_width-room_width]);
-y_start = randi([2,map_height-room_height]);
-    
-    for j=1:room_height
-        for i=1:room_width  
-            if i==1
-                map(y_start+j,x_start+i)='#'; %used on the upper border
-            elseif i==room_width
-                map(y_start+j,x_start+i)='#'; %used on lower border
-            elseif j==1
-                map(y_start+j,x_start+i)='#'; %used on left border
-            elseif j==room_height
-                map(y_start+j,x_start+i)='#'; %used on right border
+room_num=5; %determines number of rooms
+for kk=1:room_num
+    %randomly create the size of the room within 5x5 to 15x30
+    roomRows = randi([5,15]);
+    roomColumns = randi([5,30]);
+    %build the walls around the edge of the room
+    %fill the center with spaces
+    for jj=1:roomRows
+        for ii=1:roomColumns  
+            if ii==1
+                room(jj,ii)='#'; %used on the upper border
+            elseif ii==roomColumns
+                room(jj,ii)='#'; %used on lower border
+            elseif jj==1
+                room(jj,ii)='#'; %used on left border
+            elseif jj==roomRows
+                room(jj,ii)='#'; %used on right border
             else
-                map(y_start+j,x_start+i)=' '; %used everywhere else
+                room(jj,ii)=' '; %used everywhere else
             end
         end
     end
+%Save the room in a cell array
+rooms(kk)={room};
+%clear the room variable
+clear room
+end
+%% Paste the rooms into the master map
+for ii=1:numel(rooms)
+    %get the size of current room
+    [roomRow,roomColumn]=size(rooms{ii});
+    %select a starting location for the room
+    %make sure the selection cannot result in the room going over the
+    %border of the master map
+    startColumn = randi([2,mapColumns-roomColumn]);
+    startRow = randi([2,mapRows-roomRow]);
+    
+    %define the last row and column of the room within the map
+    lastRoomRow=startRow+roomRow-1;
+    lastRoomColumn=startColumn+roomColumn-1;
+    
+    %No need to check if the room fits, the selection of startRow and
+    %startColumn take into account the size of the room during selection
+    map(startRow:lastRoomRow,startColumn:lastRoomColumn)=rooms{ii};
 end
 disp(map)
 end
